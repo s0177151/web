4afcb85e2f5936f087a75e4b7ef8e93bbd091f3a
s0177151 <144156242+s0177151@users.noreply.github.com>
2024-07-04 15:04:24 +0300

    Add files via upload

--- task_7/index.php
+++ task_7/index.php
@@ -25,7 +25,7 @@
     if($vals == 2) setcookie($cook, '', 100000);
   }
   function del_cook_all($p = 0){
-    del_cook('fullName', $p);
+    del_cook('fio', $p);
     del_cook('phone', $p);
     del_cook('email', $p);
     del_cook('birthday', $p);
@@ -56,7 +56,7 @@
     }
 
     $csrf_error = (isset($_COOKIE['csrf_error']) ? checkInput($_COOKIE['csrf_error']) : '');
-    $fullName = (isset($_COOKIE['fullName_error']) ? checkInput($_COOKIE['fullName_error']) : '');
+    $fio = (isset($_COOKIE['fio_error']) ? checkInput($_COOKIE['fio_error']) : '');
     $phone = (isset($_COOKIE['phone_error']) ? checkInput($_COOKIE['phone_error']) : '');
     $email = (isset($_COOKIE['email_error']) ? checkInput($_COOKIE['email_error']) : '');
     $birthday = (isset($_COOKIE['birthday_error']) ? checkInput($_COOKIE['birthday_error']) : '');
@@ -104,7 +104,7 @@
       }
     }
     
-    val_empty('fullName', $fullName);
+    val_empty('fio', $fio);
     val_empty('phone', $phone);
     val_empty('email', $email);
     val_empty('birthday', $birthday);
@@ -131,7 +131,7 @@
           foreach($dbL->fetchAll(PDO::FETCH_ASSOC) as $item){
             $like_langsa[] = $item['name'];
           }
-          setVal('fullName', $fet['fullName']);
+          setVal('fio', $fet['fio']);
           setVal('phone', $fet['phone']);
           setVal('email', $fet['email']);
           setVal('birthday', date("Y-m-d", $fet['birthday']));
@@ -158,7 +158,7 @@
   }
   else {
     $csrf_tokens = (isset($_POST['csrf_token']) ? $_POST['csrf_token'] : '');
-    $fullName = (isset($_POST['fullName']) ? $_POST['fullName'] : '');
+    $fio = (isset($_POST['fio']) ? $_POST['fio'] : '');
     $phone = (isset($_POST['phone']) ? $_POST['phone'] : '');
     $email = (isset($_POST['email']) ? $_POST['email'] : '');
     $birthday = (isset($_POST['birthday']) ? $_POST['birthday'] : '');
@@ -204,9 +204,9 @@
       return $res;
     }
     
-    if(!val_empty('fullName', 'Заполните поле', empty($fullName))){
-      if(!val_empty('fullName', 'Длина поля > 255 символов', strlen($fullName) > 255)){
-        val_empty('fullName', 'Поле не соответствует требованиям: <i>Фамилия Имя (Отчество)</i>, кириллицей', !preg_match('/^([а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+)( [а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+){1,2}$/Diu', $fullName));
+    if(!val_empty('fio', 'Заполните поле', empty($fio))){
+      if(!val_empty('fio', 'Длина поля > 255 символов', strlen($fio) > 255)){
+        val_empty('fio', 'Поле не соответствует требованиям: <i>Фамилия Имя (Отчество)</i>, кириллицей', !preg_match('/^([а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+)( [а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+){1,2}$/Diu', $fio));
       }
     }
     if(!val_empty('phone', 'Заполните поле', empty($phone))){
@@ -254,8 +254,8 @@
     }
     
     if ($log) {
-      $stmt = $db->prepare("UPDATE form_data SET fullName = ?, phone = ?, email = ?, birthday = ?, gender = ?, biography = ? WHERE user_id = ?");
-      $stmt->execute([$fullName, $phone, $email, strtotime($birthday), $gender, $biography, $uid]);
+      $stmt = $db->prepare("UPDATE form_data SET fio = ?, phone = ?, email = ?, birthday = ?, gender = ?, biography = ? WHERE user_id = ?");
+      $stmt->execute([$fio, $phone, $email, strtotime($birthday), $gender, $biography, $uid]);
 
       $stmt = $db->prepare("DELETE FROM form_data_lang WHERE id_form = ?");
       $stmt->execute([$_SESSION['form_id']]);
@@ -278,8 +278,8 @@
         $stmt->execute([$login, $mpassword]);
         $user_id = $db->lastInsertId();
 
-        $stmt = $db->prepare("INSERT INTO form_data (user_id, fullName, phone, email, birthday, gender, biography) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$user_id, $fullName, $phone, $email, strtotime($birthday), $gender, $biography]);
+        $stmt = $db->prepare("INSERT INTO form_data (user_id, fio, phone, email, birthday, gender, biography) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$user_id, $fio, $phone, $email, strtotime($birthday), $gender, $biography]);
         $fid = $db->lastInsertId();
 
         $stmt1 = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, ?)");
@@ -291,7 +291,7 @@
         print('Error : ' . $e->getMessage());
         exit();
       }
-      set_cook('fullName_value', $fullName, 365);
+      set_cook('fio_value', $fio, 365);
       set_cook('phone_value', $phone, 365);
       set_cook('email_value', $email, 365);
       set_cook('birthday_value', $birthday, 365);
