4cf05bb1375aa490c3bcf1eb001b8e17d8cb4153
s0177151 <144156242+s0177151@users.noreply.github.com>
2024-07-04 14:45:59 +0300

    Add files via upload

---
+++ task_5/form.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Задание 5</title>
+</head>
+<body class="bg-gray-900 h-screen flex items-center justify-center">
+<div class="flex flex-col gap-2 border-2 border-white/10 rounded-xl text-white p-4 w-1/4">
+    <form  class="flex flex-col gap-2" method="post">
+        <h3 class="text-3xl font-semibold text-center">Форма</h3>
+        <div class="message"><?php if(isset($messages['success'])) echo $messages['success']; ?></div>
+        <div class="message message_info"><?php if(isset($messages['info'])) echo $messages['info']; ?></div>
+        <div class="flex flex-col gap-1">
+            <p class="text-xs text-white/20">Фамилия, имя и отчество</p>
+            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['fullName'] != NULL) ? 'borred' : ''; ?>" value="<?php echo $values['fullName']; ?>" type="text" name="fullName" placeholder="Введите фамилию, имя и отчество...">
+            <div><?php echo $messages['fio']?></div>
+        </div>
+        <div class="flex flex-col gap-1">
+            <p class="text-xs text-white/20">Номер мобильного телефона</p>
+            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['phone'] != NULL) ? 'borred' : ''; ?>" value="<?php echo $values['phone']; ?>" type="tel" name="phone" placeholder="Введите номер телефона...">
+            <div><?php echo $messages['phone']?></div>
+        </div>
+        <div class="flex flex-col gap-1">
+            <p class="text-xs text-white/20">Адрес электронной почты</p>
+            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['email'] != NULL) ? 'borred' : ''; ?>" value="<?php echo $values['email']; ?>" type="email" name="email" placeholder="Введите адрес электронной почты...">
+            <div><?php echo $messages['email']?></div>
+        </div>
+        <div class="flex flex-col gap-1">
+            <p class="text-xs text-white/20">Дата рождения</p>
+            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['birthday'] != NULL) ? 'borred' : ''; ?>" value="<?php if(strtotime($values['birthday']) > 100000) echo $values['birthday']; ?>" type="date" name="birthday">
+            <div><?php echo $messages['birthday']?></div>
+        </div>
+        <div class="ent">
+            <p class="text-xs text-white/20">Пол</p>
+            <div class="flex gap-2 text-sm"><label>
+                    <input type="radio" name="gender"
+                           value="male" <?php if ($values["gender"] == "male") {
+                        echo "checked";
+                    } ?>>
+                    <span class="<?php echo $errors["gender"] != null
+                        ? "colred"
+                        : ""; ?>">Мужской</span>
+                </label>
+                <br>
+                <label>
+                    <input type="radio" name="gender"
+                           value="female" <?php if (
+                        $values["gender"] == "female"
+                    ) {
+                        echo "checked";
+                    } ?>>
+                    <span class="<?php echo $errors["gender"] != null
+                        ? "colred"
+                        : ""; ?>">Женский</span>
+                </label>
+                <div><?php echo $messages["gender"]; ?></div>
+            </div>
+        </div>
+        <div class="ent">
+            <select class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['like_lang'] != NULL) ? 'borred' : ''; ?>" name="like_lang[]" id="like_lang" multiple="multiple">
+                <option disabled class="text-xs">Любимый язык программирования</option>
+                <option value="Pascal" <?php echo (in_array('Pascal', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Pascal</option>
+                <option value="C" <?php echo (in_array('C', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>C</option>
+                <option value="C++" <?php echo (in_array('C++', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>C++</option>
+                <option value="JavaScript" <?php echo (in_array('JavaScript', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>JavaScript</option>
+                <option value="PHP" <?php echo (in_array('PHP', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>PHP</option>
+                <option value="Python" <?php echo (in_array('Python', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Python</option>
+                <option value="Java" <?php echo (in_array('Java', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Java</option>
+                <option value="Haskel" <?php echo (in_array('Haskel', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Haskel</option>
+                <option value="Clojure" <?php echo (in_array('Clojure', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Clojure</option>
+                <option value="Prolog" <?php echo (in_array('Prolog', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Prolog</option>
+                <option value="Scala" <?php echo (in_array('Scala', $favoriteLanguagesSA)) ? 'selected' : ''; ?>>Scala</option>
+            </select>
+            <div><?php echo $messages['like_lang']?></div>
+        </div>
+        <div>
+            <textarea name="biography" placeholder="Биография" class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['biography'] != NULL) ? 'borred' : ''; ?>"><?php echo $values['biography']; ?></textarea>
+            <div><?php echo $messages['biography']?></div>
+        </div>
+        <div>
+            <input type="checkbox" name="agreement" id="agreement" <?php echo ($values['agreement'] != NULL) ? 'checked' : ''; ?>>
+            <label for="agreement" class="text-sm <?php echo ($errors['agreement'] != NULL) ? 'colred' : ''; ?>">С контрактом ознакомлен (а)</label>
+            <div><?php echo $messages['agreement']?></div>
+        </div>
+        <?php
+            if($log) echo '<button type="submit" class="p-2 rounded-lg bg-green-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg">Изменить</button>';
+            else echo '<button type="submit" class="p-2 rounded-lg bg-green-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg">Отправить</button>';
+        ?>
+    </form>
+        <div class="mt-2 flex justify-between items-center w-full">
+            <p class="text-sm opacity-50">Войдите в аккаунт</p>
+            <?php
+            if($log) echo '<button type="submit" class="p-2 rounded-lg bg-red-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg" name="logout_form">Выйти</button>';
+            else echo '<a href="login.php" class="p-2 rounded-lg bg-gray-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg" name="logout_form">Войти</a>';
+            ?>
+        </div>
+  </div>
+</body>
+</html>
---
+++ task_5/login.php
@@ -0,0 +1,88 @@
+<?php
+
+  /**
+   * Файл login.php для не авторизованного пользователя выводит форму логина.
+   * При отправке формы проверяет логин/пароль и создает сессию,
+   * записывает в нее логин и id пользователя.
+   * После авторизации пользователь перенаправляется на главную страницу
+   * для изменения ранее введенных данных.
+   **/
+
+  // Отправляем браузеру правильную кодировку,
+  // файл login.php должен быть в кодировке UTF-8 без BOM. 
+  header('Content-Type: text/html; charset=UTF-8');
+  session_start();
+  // В суперглобальном массиве $_SESSION хранятся переменные сессии.
+  // Будем сохранять туда логин после успешной авторизации.
+  if (!empty($_SESSION['login'])) {
+    // Если есть логин в сессии, то пользователь уже авторизован.
+    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
+    //при нажатии на кнопку Выход).
+    // Делаем перенаправление на форму.
+    header('Location: ./');
+    exit();
+  }
+
+
+  $error = '';
+
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+  }
+  else{
+    // TODO: Проверть есть ли такой логин и пароль в базе данных.
+    // Выдать сообщение об ошибках.
+    require('connection.php');
+    $login = $_POST['login'];
+    $password = md5($_POST['password']);
+    try {
+      $stmt = $db->prepare("SELECT id FROM users WHERE login = ? and password = ?");
+      $stmt->execute([$login, $password]);
+      $its = $stmt->rowCount();
+      if($its){
+        $uid = $stmt->fetchAll(PDO::FETCH_ASSOC)[0]['id'];
+        $_SESSION['login'] = $_POST['login'];
+        $_SESSION['user_id'] = $uid;
+        // Делаем перенаправление.
+        header('Location: ./');
+      }
+      else{
+        $error = 'Неверный логин или пароль';
+      }
+    }
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+      exit();
+    }
+  }
+
+  // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+  // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Задание 7</title>
+</head>
+<body class="bg-gray-900 h-screen flex items-center justify-center">
+  <div class="flex flex-col gap-2 border-2 border-white/10 rounded-xl text-white p-4 w-1/4">
+    <form class="flex flex-col gap-2" method="post">
+      <div class="message" style="color: red;"><?php echo $error; ?></div>
+        <h3 class="text-3xl font-semibold text-center">Авторизация</h3>
+        <div>
+          <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full" type="text" name="login" placeholder="Логин">
+        </div>
+        <div>
+          <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full" type="text" name="password" placeholder="Пароль">
+        </div>
+        <button type="submit" class="p-2 rounded-lg bg-green-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg">Войти</button>
+    </form>
+  </div>
+</body>
+</html>
