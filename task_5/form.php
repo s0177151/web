73adf87a87672092fecf4bc2d83da299a52ca659
s0177151 <144156242+s0177151@users.noreply.github.com>
2024-07-04 14:42:47 +0300

    Add files via upload

--- task_5/form.php
+++ task_5/form.php
@@ -15,7 +15,7 @@
         <div class="message message_info"><?php if(isset($messages['info'])) echo $messages['info']; ?></div>
         <div class="flex flex-col gap-1">
             <p class="text-xs text-white/20">Фамилия, имя и отчество</p>
-            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['fio'] != NULL) ? 'borred' : ''; ?>" value="<?php echo $values['fio']; ?>" type="text" name="fio" placeholder="Введите фамилию, имя и отчество...">
+            <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full <?php echo ($errors['fullName'] != NULL) ? 'borred' : ''; ?>" value="<?php echo $values['fullName']; ?>" type="text" name="fullName" placeholder="Введите фамилию, имя и отчество...">
             <div><?php echo $messages['fio']?></div>
         </div>
         <div class="flex flex-col gap-1">
--- task_5/index.php
+++ task_5/index.php
@@ -211,7 +211,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 $_SESSION['form_id'] = $form_id;
                 if (!empty($like_lang)) {
                     foreach ($like_lang as $lang) {
-                        $dbL = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, IFNULL((SELECT id FROM languages WHERE name = ?), 0))");
+                        $dbL = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, (SELECT id FROM languages WHERE name = ?))");
                         $dbL->execute([$form_id, $lang]);
                     }
                 }
