d4013818847aaa238d0b04860690a7d24631ef02
s0177151 <144156242+s0177151@users.noreply.github.com>
2024-07-11 03:06:28 +0300

    Add files via upload

--- task_5/connection.php
+++ task_5/connection.php
@@ -1,4 +1,4 @@
 <?php
-    db = new PDO('mysql:host=localhost;dbname=u67495', 'u67495', '2783790',
+    $db = new PDO('mysql:host=localhost;dbname=u67495', 'u67495', '2783790',
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>
